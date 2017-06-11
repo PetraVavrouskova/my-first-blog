ca15d323c1985b361cbf7c0ac91aca8eb9e9dcca
PetraVavrouskova <petra.vavrouskova@gmail.cz>
2017-06-11 16:04:01 +0200

    Modified templates.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post    #Tečka po  from  znamená aktuální adresář nebo aktuální aplikace. Protože se  views.py  a  models.py  nachází ve stejném adresáři, můžeme jednoduše použít  .  a jméno souboru (bez  .py ).
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
